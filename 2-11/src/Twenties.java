b9166d1c4dc179f04e04d92d71e454edb00a58a5
junyong <jymoon1121@gmail.com>
2022-06-28 16:32:05 +0900

    Update Chapter 2

---
+++ 2-11/src/Twenties.java
@@ -0,0 +1,18 @@
+// if-else문 예시
+import java.util.Scanner;
+public class Twenties {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("나이를 입력하시오:");
+		int age = scanner.nextInt(); // 정수 입력
+		if((age>=20) && (age<30)) { // age가 20~29 사이인지 검
+			System.out.print("20대 입니다. ");
+			System.out.println("20대라서 행복합니다!");			
+		}
+		else 
+			System.out.println("20대가 아닙니다.");
+		
+		scanner.close();
+	}
+}
